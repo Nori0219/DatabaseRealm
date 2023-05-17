23c0f3463ecb8f651bc66e735bf90eadb50b5bb5
NorichikaSugii <wawa60379@gmail.com>
2023-05-17 16:34:43 +0900

    Initial Commit

---
+++ DatabaseRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DatabaseRealm
+//
+//  Created by 杉井位次 on 2023/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
